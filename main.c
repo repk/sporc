a716cf7d29b609c8347866fdfbd62c9e9ea38e18
Remi Pommarel <repk@triplefau.lt>
2017-07-04 10:20:45 +0200

    [sparc] Fix main binary search path

--- main.c
+++ main.c
@@ -1,23 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "cpu.h"
 #include "memory.h"
+#include "utils.h"
 
 #define PROGFILE "./example/example.bin"
 #define KB 1024
 #define MEMSZ (250 * KB)
+
+int get_file_path(int argc, char **argv, char *file,
+		char *path, size_t sz)
+{
+	char *ptr;
+
+	if(argc == 0)
+		return -1;
+
+	ptr = strrchr(argv[0], '/');
+	if(ptr) {
+		snprintf(path, sz - 1, "%.*s/%s", (int)(ptr - argv[0]),
+				argv[0], file);
+	} else {
+		strncpy(path, file, sz - 1);
+	}
+	path[sz - 1] = '\0';
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
 	struct cpu *cpu;
 	struct memory *mem;
 	int ret;
+	char f[FILENAME_MAX];
 
-	(void)argc;
-	(void)argv;
+	ret = get_file_path(argc, argv, PROGFILE, f, ARRAY_SIZE(f));
+	if(ret != 0) {
+		fprintf(stderr, "Cannot get file path\n");
+		return -1;
+	}
 
-	mem = memory_create("file-mem", PROGFILE);
+	mem = memory_create("file-mem", f);
 	if(mem == NULL) {
 		fprintf(stderr, "Cannot create memory segment\n");
 		return -1;
