c9ab0275ee28424654007977789a990c9df0c72f
Remi Pommarel <repk@triplefau.lt>
2017-07-26 21:16:41 +0200

    [cpu] Add comment for cpu subsystem

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -2,6 +2,12 @@
 
 #include "cpu.h"
 
+/**
+ * Find a registered cpu plugin from its name.
+ *
+ * @param name: Name of requested cpu plugin
+ * @return: The cpu plugin descriptor if found, NULL pointer otherwise
+ */
 static struct cpu_desc const *cpu_desc_get(char const *name)
 {
 	struct cpu_desc const **c, *ret;
@@ -18,26 +24,57 @@ static struct cpu_desc const *cpu_desc_get(char const *name)
 	return ret;
 }
 
+/**
+ * Fetch next cpu instruction
+ *
+ * @param c: cpu instance
+ * @return: 0 on success, -1 otherwise
+ */
 int cpu_fetch(struct cpu *c)
 {
 	return c->cpu->cops->fetch(c);
 }
 
+/**
+ * Decode next cpu instruction
+ *
+ * @param c: cpu instance
+ * @return: 0 on success, -1 otherwise
+ */
 int cpu_decode(struct cpu *c)
 {
 	return c->cpu->cops->decode(c);
 }
 
+/**
+ * Exec next cpu instruction
+ *
+ * @param c: cpu instance
+ * @return: 0 on success, -1 otherwise
+ */
 int cpu_exec(struct cpu *c)
 {
 	return c->cpu->cops->exec(c);
 }
 
+/**
+ * Boot a specific cpu
+ *
+ * @param c: cpu instance to boot
+ * @return: 0 on success, -1 otherwise
+ */
 int cpu_boot(struct cpu *c, uintptr_t addr)
 {
 	return c->cpu->cops->boot(c, addr);
 }
 
+/**
+ * Instantiate a cpu plugin subsystem.
+ *
+ * @param name: Name of requested cpu plugin
+ * @param args: cpu plugin specific arguments
+ * @return: NULL pointer on error, a cpu plugin instance otherwise
+ */
 struct cpu *cpu_create(char const *name, struct memory *mem, char const *args)
 {
 	struct cpu_desc const *cdesc;
@@ -56,6 +93,11 @@ struct cpu *cpu_create(char const *name, struct memory *mem, char const *args)
 	return c;
 }
 
+/**
+ * Destroy a cpu plugin instance.
+ *
+ * @param m: plugin instance.
+ */
 int cpu_destroy(struct cpu *c)
 {
 	c->cpu->cops->destroy(c);
--- include/cpu.h
+++ include/cpu.h
@@ -5,26 +5,70 @@
 
 struct cpu_desc;
 
+/**
+ * Cpu operations
+ */
 struct cpu_ops {
+	/**
+	 * Plugin instantiation
+	 */
 	struct cpu *(*create)(char const *args);
+	/**
+	 * Plugin instance destruction
+	 */
 	void (*destroy)(struct cpu *cpu);
+	/**
+	 * Boot this cpu at specific address
+	 */
 	int (*boot)(struct cpu *cpu, uintptr_t addr);
-	/* Interrupt */
+
+	/* TODO addInterrupt */
+
+	/**
+	 * Instruction fetch operation
+	 */
 	int (*fetch)(struct cpu *cpu);
+	/**
+	 * Instruction decode operation
+	 */
 	int (*decode)(struct cpu *cpu);
+	/**
+	 * Instruction execution operation
+	 */
 	int (*exec)(struct cpu *cpu);
 };
 
+/**
+ * Cpu plugin descriptor
+ */
 struct cpu_desc {
+	/**
+	 * Cpu plugin name used as id
+	 */
 	char *name;
+	/**
+	 * Plugin operations
+	 */
 	struct cpu_ops const *cops;
 };
 
+/**
+ * Cpu instance structure
+ */
 struct cpu {
+	/**
+	 * Plugin descriptor
+	 */
 	struct cpu_desc const *cpu;
+	/**
+	 * Cpu memory map
+	 */
 	struct memory *mem;
 };
 
+/**
+ * Register a cpu plugin
+ */
 #define CPU_REGISTER(c)							\
 	__attribute__((section(".rodata.cpu"), used))			\
 	static struct cpu_desc const * const __cpu_ ## c = &c
